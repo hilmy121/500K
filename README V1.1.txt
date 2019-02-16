e5b25d2081154e545ec8e563130b68f7e65d5184
Akhmad Hilmy Zakaria <caklunim@gmail.com>
2019-02-16 09:24:08 +0700

    Second commit

--- README V1.1.txt
+++ README V1.1.txt
@@ -1,4 +1,7 @@
 - Logika ViewPager belom di update
 - Asset belom di import
+- Layouting - content 
+
++ finished Wawasan layout
 NB :
 Update Readme tiap selese pull request
--- app/src/main/java/com/example/hilmy/mangatusewu/WawasanActivity.java
+++ app/src/main/java/com/example/hilmy/mangatusewu/WawasanActivity.java
@@ -2,12 +2,44 @@ package com.example.hilmy.mangatusewu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class WawasanActivity extends AppCompatActivity {
-
+    CardView cvWawasan1,cvWawasan2,cvWawasan3,cvWawasan4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wawasan);
+        cvWawasan1 = findViewById(R.id.cvWawasan1);
+        cvWawasan2 = findViewById(R.id.cvWawasan2);
+        cvWawasan3 = findViewById(R.id.cvWawasan3);
+        cvWawasan4 = findViewById(R.id.cvWawasan4);
+
+        cvWawasan1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cvWawasan2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cvWawasan3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cvWawasan4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
