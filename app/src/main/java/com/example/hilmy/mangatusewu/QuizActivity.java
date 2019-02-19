bbe8148aa5a7e738631a25c94e2664527ee3536e
Akhmad Hilmy Zakaria <caklunim@gmail.com>
2019-02-19 10:42:53 +0700

    add qioz

--- app/src/main/java/com/example/hilmy/mangatusewu/QuizActivity.java
+++ app/src/main/java/com/example/hilmy/mangatusewu/QuizActivity.java
@@ -2,12 +2,49 @@ package com.example.hilmy.mangatusewu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class QuizActivity extends AppCompatActivity {
+
+    TextView tvKuis;
+    ImageButton btnKuis;
+    String[] pertanyaan;
+    private ArrayList<Integer> urutan = new ArrayList<>();
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        tvKuis = findViewById(R.id.tvResultKuis);
+        btnKuis = findViewById(R.id.btnKuis);
+        initQuiz();
+        btnKuis.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(i>pertanyaan.length){
+                    tvKuis.setText(pertanyaan[urutan.get(i)]);
+                    i++;
+                }
+                if(i==pertanyaan.length){
+                    Collections.shuffle(urutan);
+                    i=0;
+                }
+            }
+        });
+
+    }
+    private void initQuiz () {
+        pertanyaan = getResources().getStringArray(R.array.ArrayKuis);
+        for (int id = 0; id < pertanyaan.length; id++) {
+            urutan.add(id);
+            Collections.shuffle(urutan);
+        }
     }
 }
